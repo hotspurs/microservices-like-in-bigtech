e3e5992e0fb636b328ead31374bdd852cb667d87
Vlad Dubov <dubov.web@gmail.com>
2024-08-06 22:35:56 +0300

    swaggerui on the same port

--- services/chat/main.go
+++ services/chat/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	pb "chat/pkg/api/chat"
 	"context"
+	"fmt"
 	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
 	"github.com/labstack/echo/v4"
 	"google.golang.org/grpc"
@@ -64,11 +65,6 @@ func main() {
 		if err := grpcServer.Serve(lis); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 		}
-
-		// SaveNote:
-		// grpc_cli call --json_input --json_output localhost:8082 NotesService/SaveNote '{"info":{"title":"my note","content":"my note content"}}'
-		// ListNotes:
-		// grpc_cli call --json_input --json_output localhost:8082 NotesService/ListNotes '{}'
 	}()
 
 	wg.Add(1)
@@ -80,46 +76,25 @@ func main() {
 		if err = pb.RegisterChatServiceHandlerServer(ctx, mux, server); err != nil {
 			log.Fatalf("failed to serve: %v", err)
 		}
-		httpServer := &http.Server{Handler: mux}
-
-		lis, err := net.Listen("tcp", ":8080")
-		if err != nil {
-			log.Fatalf("failed to listen: %v", err)
-		}
-
-		// Start HTTP server (and proxy calls to gRPC server endpoint)
-		log.Printf("server listening at %v", lis.Addr())
-		if err := httpServer.Serve(lis); err != nil {
-			log.Fatalf("failed to serve: %v", err)
-		}
-	}()
-
-	wg.Add(1)
-	go func() {
-		defer wg.Done()
 		e := echo.New()
-		e.Static("/docs", "./swagger")
-		e.Logger.Fatal(e.Start(":8083"))
-	}()
+		e.GET("/live", func(c echo.Context) error {
+			return c.String(http.StatusOK, "")
+		})
 
+		e.GET("/ready", func(c echo.Context) error {
+			return c.String(http.StatusOK, "")
+		})
+		e.GET("/auth", func(c echo.Context) error {
+			res, err := http.Get("http://auth:8080")
+			if err != nil {
+			}
+			fmt.Println(res)
+			return c.String(res.StatusCode, "")
+		})
+		e.Static("/docs", "./swagger")
+		e.Any("/*", echo.WrapHandler(mux))
+
+		e.Logger.Fatal(e.Start(":8080"))
+	}()
 	wg.Wait()
-	//e := echo.New()
-	//e.GET("/", func(c echo.Context) error {
-	//	return c.String(http.StatusOK, "Hello, Chat!")
-	//})
-	//e.GET("/live", func(c echo.Context) error {
-	//	return c.String(http.StatusOK, "")
-	//})
-	//
-	//e.GET("/ready", func(c echo.Context) error {
-	//	return c.String(http.StatusOK, "")
-	//})
-	//e.GET("/auth", func(c echo.Context) error {
-	//	res, err := http.Get("http://auth:8080")
-	//	if err != nil {
-	//	}
-	//	fmt.Println(res)
-	//	return c.String(res.StatusCode, "")
-	//})
-	//e.Logger.Fatal(e.Start(":8080"))
 }
